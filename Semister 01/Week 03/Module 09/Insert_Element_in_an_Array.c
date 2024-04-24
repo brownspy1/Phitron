34e9a4772b34e138f94fb7c3f58b46ce92be1db7
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-04-24 19:43:49 +0600

    add

--- Semister 01/Week 03/Module 09/Insert_Element_in_an_Array.c
+++ Semister 01/Week 03/Module 09/Insert_Element_in_an_Array.c
@@ -3,7 +3,25 @@
 //
 #include <stdio.h>
 int main(){
-    char k;
-    scanf("%s",&k);
+    int n;
+    scanf("%d",&n);
+    int arry[n+1];
+    for(int i =0; i<n; i++){
+        scanf("%d",&arry[i]);
+    }
+    int index ,value;
+    scanf("%d %d",&index ,&value);
+
+    // reorder index
+for (int i = n; i >= index+1; i--){
+        arry[i]=arry[i-1];
+        printf("%d ",i);
+    
+}
+    arry[index] = arry[value];
+
+    for(int i =0; i<=n; i++){
+        printf("%d ",arry[i]);
+    }
     return 0;
 }
