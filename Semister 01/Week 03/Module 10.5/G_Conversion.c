eb095037c34949964756298720f0d1e8aef3c4dc
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-04-25 23:31:24 +0600

    add

---
+++ Semister 01/Week 03/Module 10.5/G_Conversion.c
@@ -0,0 +1,46 @@
+// Created by M.Mahadi on 2024-04-25 22:36:19
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char arry[100000];
+    fgets(arry,100000,stdin);
+
+    for (int i = 0; i < strlen(arry); i++){
+        if (arry[i]>='a' && arry[i]<='z'){
+            arry[i] = arry[i]-32;
+        }else if (arry[i]>='A' && arry[i]<='Z')
+        {
+            arry[i]=arry[i]+32;
+        }else if (arry[i]==',')
+        {
+            arry[i]=' ';
+        }
+        printf("%c",arry[i]);
+        
+        
+    }
+        
+    
+    return 0;
+}
+
+// #include <stdio.h>
+// #include <string.h>
+
+// int main() {
+//     char arry[100000];
+//     fgets(arry, 100000, stdin);
+
+//     for (int i = 0; i < strlen(arry); i++) {
+//         if (arry[i] == ',') {
+//             arry[i] = ' ';
+//         } else if (arry[i] >= 'a' && arry[i] <= 'z') {
+//             arry[i] -= 32; 
+//         } else if (arry[i] >= 'A' && arry[i] <= 'Z') {
+//             arry[i] += 32; 
+//         }
+//         printf("%c", arry[i]); // Print the character
+//     }
+
+//     return 0;
+// }
