f4193ccd7caf2eecc45aac84241b6053e594d7b6
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-04-25 13:55:17 +0600

    add

--- Semister 01/Week 03/Module 10/String.c
+++ Semister 01/Week 03/Module 10/String.c
@@ -1,6 +1,19 @@
 // Created by M.Mahadi on 2024-04-25 13:14:30
 #include <stdio.h>
 int main(){
+    int n;
+    scanf("%d",&n);
+    char arry[n];
+    for (int i = 0; i <= n; i++)
+    {
+        scanf("%c ",&arry[i]);
+    }
+
+    for (int i = 0; i <= n; i++)
+    {
+        printf("%c",arry[i]);
+    }
     
+
     return 0;
 }
