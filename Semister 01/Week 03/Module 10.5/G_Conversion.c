23c1c621888e5e75a997c39fd68fd2dfd8832382
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-04-26 01:51:45 +0600

    add

--- Semister 01/Week 03/Module 10.5/G_Conversion.c
+++ Semister 01/Week 03/Module 10.5/G_Conversion.c
@@ -1,4 +1,5 @@
 // Created by M.Mahadi on 2024-04-25 22:36:19
+//solve hoy nai
 #include <stdio.h>
 #include <string.h>
 int main(){
@@ -6,41 +7,16 @@ int main(){
     fgets(arry,100000,stdin);
 
     for (int i = 0; i < strlen(arry); i++){
-        if (arry[i]>='a' && arry[i]<='z'){
+        if (arry[i]==',')
+        {
+            arry[i]=' ';
+        }else if (arry[i]>='a' && arry[i]<='z'){
             arry[i] = arry[i]-32;
         }else if (arry[i]>='A' && arry[i]<='Z')
         {
             arry[i]=arry[i]+32;
-        }else if (arry[i]==',')
-        {
-            arry[i]=' ';
         }
-        printf("%c",arry[i]);
-        
-        
+        printf("%c",arry[i]);   
     }
-        
-    
     return 0;
 }
-
-// #include <stdio.h>
-// #include <string.h>
-
-// int main() {
-//     char arry[100000];
-//     fgets(arry, 100000, stdin);
-
-//     for (int i = 0; i < strlen(arry); i++) {
-//         if (arry[i] == ',') {
-//             arry[i] = ' ';
-//         } else if (arry[i] >= 'a' && arry[i] <= 'z') {
-//             arry[i] -= 32; 
-//         } else if (arry[i] >= 'A' && arry[i] <= 'Z') {
-//             arry[i] += 32; 
-//         }
-//         printf("%c", arry[i]); // Print the character
-//     }
-
-//     return 0;
-// }
