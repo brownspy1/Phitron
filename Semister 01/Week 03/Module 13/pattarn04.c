9dad8cb75f5c0730f89800d645fb929ac4755b45
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-04-28 18:39:20 +0600

    add

---
+++ Semister 01/Week 03/Module 13/pattarn04.c
@@ -0,0 +1,33 @@
+// Created by M.Mahadi on 2024-04-28 17:43:48
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int k=1;
+    int j = n-1;
+    for (int i = 1; i <= (2*n)-1; i++)
+    {
+        for (int s = 1; s <= j; s++)
+        {
+            printf(" ");
+        }
+        for (int s = 1; s <= k; s++)
+        {
+            printf("%d",s);
+        }
+        if (i<=n-1)
+        {
+        j--;
+        k+=2;
+        }else
+        {
+            j++;
+            k-=2;
+        }
+        
+        
+        printf("\n");
+    }
+    return 0;
+}
