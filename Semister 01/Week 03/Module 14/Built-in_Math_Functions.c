8d1d3ab65559593619aa2b50e73505c0d85079aa
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-04-30 21:43:04 +0600

    add

---
+++ Semister 01/Week 03/Module 14/Built-in_Math_Functions.c
@@ -0,0 +1,6 @@
+// Created by M.Mahadi on 2024-04-30 21:42:42
+#include <stdio.h>
+int main(){
+    
+    return 0;
+}
---
+++ Semister 01/Week 03/Module 14/No_Return+Parameter.c
@@ -0,0 +1,6 @@
+// Created by M.Mahadi on 2024-04-30 21:03:01
+#include <stdio.h>
+int main(){
+    
+    return 0;
+}
