d4153aa6033a1e5685326b29f9ecd036c3f60899
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-05-03 20:14:38 +0600

    add

---
+++ Semister 01/Week 04/Assaiinment 03/1.c
@@ -0,0 +1,12 @@
+// Created by M.Mahadi on 2024-05-03 20:02:20
+#include <stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    for (int i = n; i > 1; i++)
+    {
+        printf("%d",i);
+    }
+    
+    return 0;
+}
