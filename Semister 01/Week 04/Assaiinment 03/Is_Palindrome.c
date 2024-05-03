60dec1bd3c33ff5bffb72ac2addc5365abd0167e
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-05-04 02:33:38 +0600

    add

--- Semister 01/Week 04/Assaiinment 03/Is_Palindrome.c
+++ Semister 01/Week 04/Assaiinment 03/Is_Palindrome.c
@@ -1,8 +1,33 @@
 // Created by M.Mahadi on 2024-05-03 21:16:37
 #include <stdio.h>
+#include <string.h>
 int main(){
     int arry[1001];
     scanf("%s",&arry);
-    printf("%s",arry);
+    int count = strlen(arry);
+    int x=count-1;
+    int flag=1;
+    for (int i = 0; x >i; i++,x--)
+    {
+        // if (arry[i]!=arry[x])
+        // {
+        //     flag=0;
+        //     break;
+        // }
+        printf("%c %c\n",arry[i],arry[x]);
+    }
+    // if (flag==0)
+    // {
+    //     printf("Not Palindrome");
+    // }else if (flag==1)
+    // {
+    // printf("Palindrome");
+    // }
+    
+    
+    
+   
+    
+    
     return 0;
 }
