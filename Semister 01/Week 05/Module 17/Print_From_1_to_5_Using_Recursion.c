74c4a05cf2fa9095eb0b817712d769a0bb647160
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-05-17 20:01:52 +0600

    add

--- Semister 01/Week 05/Module 17/Print_From_1_to_5_Using_Recursion.c
+++ Semister 01/Week 05/Module 17/Print_From_1_to_5_Using_Recursion.c
@@ -1,6 +1,14 @@
 // Created by M.Mahadi on 2024-05-13 18:07:27
 #include <stdio.h>
-int main(){
+void recarson(int i){
+    if (i>1) return;
+    printf(" %d\n",i);
+    recarson(i-1);
+   
     
+}
+int main(){
+    recarson(6);
     return 0;
 }
+
---
+++ c.c
@@ -0,0 +1,6 @@
+// Created by M.Mahadi on 2024-05-16 16:03:26
+#include <stdio.h>
+int main(){
+    printf("hello world");
+    return 0;
+}
