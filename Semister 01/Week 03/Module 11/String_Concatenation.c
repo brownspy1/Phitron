834cb0c649f88cb5468db4120ee17bbe9d45e6c8
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-05-20 18:29:38 +0600

    add

---
+++ Semister 01/Week 03/Module 11/String_Concatenation.c
@@ -0,0 +1,8 @@
+// Created by M.Mahadi on 2024-05-20 18:24:29
+#include <stdio.h>
+int main(){
+    int n;
+    
+    int a[]
+    return 0;
+}
--- Semister 01/Week 03/Module 11/String_Lexicographical_Comparison.c
+++ Semister 01/Week 03/Module 11/String_Lexicographical_Comparison.c
@@ -1,5 +1,6 @@
 // Created by M.Mahadi on 2024-05-20 11:18:09
 #include <stdio.h>
+#include <string.h>
 int compare(char a[],char b[]){
     for (int i = 0;; i++)
     {
@@ -10,27 +11,44 @@ int compare(char a[],char b[]){
         }
         else if (a[i]=='\0')
         {
-            return 1;
+            return -1;
             break;
         }
         else if (b[i]=='\0')
         {
-            return 2;
+            return 1;
             break;
         }
         if (a[i]<b[i])
         {
-            return 1;
+            return -1;
+            break;
         }else if (b[i]<a[i])
         {
-            return 2;
+            return 1;
+            break;
         }
     }
 }
 
 int main(){
     char a[100],b[100];
-    int ans = compare(a,b);
-    printf("%d",ans);
+    scanf("%s %s",&a,&b);
+    int ans = compare(a,b); // usar defain funtion
+    // int ans = strcmp(a,b); // string.h hedae building funtion
+
+    if (ans == 0)
+    {
+        printf("EQULE\n");
+    }else if (ans < 0)
+    {
+        printf("B\n");
+    }else if (ans > 0)
+    {
+        printf("A\n");
+    }
+    
+    
+    
     return 0;
 }
