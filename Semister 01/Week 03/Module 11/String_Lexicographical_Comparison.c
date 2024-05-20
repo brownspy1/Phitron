1769e28b1dadfa0b2ddb2b4e37985fcf01dc1abe
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-05-20 17:47:35 +0600

    add

--- Semister 01/Week 03/Module 11/String_Lexicographical_Comparison.c
+++ Semister 01/Week 03/Module 11/String_Lexicographical_Comparison.c
@@ -1,33 +1,32 @@
 // Created by M.Mahadi on 2024-05-20 11:18:09
 #include <stdio.h>
 int compare(char a[],char b[]){
-    for (int i = 0; ; i++)
+    for (int i = 0;; i++)
     {
-        if (a[i]=='\0' && b[i]=='\0')
+        if (a[i]=='/0' && b[i]=='\0')
         {
             return 0;
             break;
         }else if (a[i]=='\0')
         {
-            return 2;
+            return 1;
             break;
         }else if (b[i]=='\0')
         {
-            return 1;
+            return 2;
             break;
         }
 
         if (a[i]<b[i])
         {
-            return 2;
-            break;
+            return 1;
         }else if (b[i]<a[i])
         {
-            return 1;
-            break;
-        }  
+            return 2;
+        }
     }
 }
+
 int main(){
     char a[100],b[100];
     int ans = compare(a,b);
