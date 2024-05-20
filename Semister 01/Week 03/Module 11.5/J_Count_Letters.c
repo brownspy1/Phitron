9a6ab553c8d02d8035d23b73c4dbca530a1cb7aa
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-05-21 03:35:31 +0600

    add

---
+++ Semister 01/Week 03/Module 11.5/J_Count_Letters.c
@@ -0,0 +1,18 @@
+// Created by M.Mahadi on 2024-05-21 03:07:25
+#include <stdio.h>
+#include <string.h>
+int compear(char a[],char b[]){
+    
+    
+}
+int main(){
+    int A[10000000];
+    scanf("%d ",&A);
+    int count[26];
+    for (int i = 0; i < strlen(A); i++)
+    {
+        
+    }
+    
+    return 0;
+}
--- Semister 01/Week 03/Module 11.5/K_Sum_Digits.c
+++ Semister 01/Week 03/Module 11.5/K_Sum_Digits.c
@@ -1,17 +1,14 @@
-// Created by M.Mahadi on 2024-05-21 00:57:48
+// Created by M.Mahadi on 2024-05-21 02:49:06
 #include <stdio.h>
 int main(){
     int n;
     scanf("%d",&n);
-    int arry[n];
+    char arry[n+1];
+    scanf("%s",arry);
+    int add =0;
     for (int i = 0; i < n; i++)
     {
-        scanf("%d",&arry[i]);
-    }
-    int add = 0;
-    for (int i = 0; i < n; i++)
-    {
-        add +=arry[i]-'0';
+        add += arry[i]-'0'; 
     }
     printf("%d",add);
     return 0;
