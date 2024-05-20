1d1fed02018aed0ac0a6f3b59438aa7f44854919
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-05-21 04:20:42 +0600

    add

--- Semister 01/Week 03/Module 11.5/J_Count_Letters.c
+++ Semister 01/Week 03/Module 11.5/J_Count_Letters.c
@@ -1,18 +1,47 @@
-// Created by M.Mahadi on 2024-05-21 03:07:25
+// // Created by M.Mahadi on 2024-05-21 03:07:25
+// #include <stdio.h>
+// #include <string.h>
+// int main(){
+//     char arry[1000001];
+//     scanf("%s",arry);
+//     int count[26] = {0};
+//     int lenth = strlen(arry);
+//     for (int i = 0; i < lenth; i++)
+//     {
+//         count[arry[i]-'a']++;
+//     }
+//  for (int i = 0; i < 26; i++)
+//     {
+//         if (count[i]!=0)
+//         {
+//             printf( "%c : %d\n",i+'a',count[i]);
+//         }
+        
+//     }
+    
+//     return 0;
+// }
+
+// Created by M.Mahadi on 2024-05-20 20:54:02
 #include <stdio.h>
 #include <string.h>
-int compear(char a[],char b[]){
-    
-    
-}
 int main(){
-    int A[10000000];
-    scanf("%d ",&A);
-    int count[26];
-    for (int i = 0; i < strlen(A); i++)
-    {
-        
-    }
     
+    char arry[101];
+    scanf("%s",arry);
+    int count[26] = {0};
+    for (int i = 0; i < strlen(arry); i++)
+    {
+        count[arry[i]-'a']++;
+    }
+
+    for (int i = 0; i < 26; i++)
+    {
+       if (count[i]!=0)
+       {
+         printf("%c : %d\n",i+'a',count[i]);  
+       }
+         
+    }
     return 0;
 }
