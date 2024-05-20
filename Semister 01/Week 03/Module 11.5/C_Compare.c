d0ba6d2149ad6d7fcd3a0fa1dfa45418adc63238
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-05-20 23:21:54 +0600

    add

---
+++ Semister 01/Week 03/Module 11.5/C_Compare.c
@@ -0,0 +1,16 @@
+// Created by M.Mahadi on 2024-05-20 23:13:00
+#include <stdio.h>
+#include()
+int compere(char X[],char Y[]){
+    for (int i = 0; i < ; i++)
+    {
+        /* code */
+    }
+    
+}
+int main(){
+    char X[21],Y[21];
+    scanf("%s \n %s",&X,&Y);
+    printf("%s \n%s",X,Y);
+    return 0;
+}
