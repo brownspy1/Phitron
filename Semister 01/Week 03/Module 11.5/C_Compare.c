ad6634cd22f8efdb3be9950a029c2198e72fc3df
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-05-21 00:58:53 +0600

    add

--- Semister 01/Week 03/Module 11.5/C_Compare.c
+++ Semister 01/Week 03/Module 11.5/C_Compare.c
@@ -1,16 +1,45 @@
 // Created by M.Mahadi on 2024-05-20 23:13:00
 #include <stdio.h>
-#include()
 int compere(char X[],char Y[]){
-    for (int i = 0; i < ; i++)
+    for (int i = 0; ; i++)
     {
-        /* code */
+        if (X[i]=='\0' && Y[i]=='\0')
+        {
+            return 0;
+        }else if (X[i]=='\0')
+        {
+            return -1;
+        }else if (Y[i]=='\0')
+        {
+            return 1;
+        }
+        if (X[i]<Y[i])
+        {
+            return -1;
+        }else if (Y[i]<X[i])
+        {
+            return 1;
+        }
+
     }
     
 }
 int main(){
     char X[21],Y[21];
     scanf("%s \n %s",&X,&Y);
-    printf("%s \n%s",X,Y);
+    int val = compere(X,Y);
+    if (val<0)
+    {
+        printf("%s",X);
+    }else if (val>0)
+    {
+        printf("%s",Y);
+    }else if (val == 0)
+    {
+        printf("%s",X);
+    }
+    
+    
+    
     return 0;
 }
---
+++ Semister 01/Week 03/Module 11.5/K_Sum_Digits.c
@@ -0,0 +1,12 @@
+// Created by M.Mahadi on 2024-05-21 00:57:48
+#include <stdio.h>
+int main(){
+    int n;
+    int arry[n+1];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d",);
+    }
+    
+    return 0;
+}
