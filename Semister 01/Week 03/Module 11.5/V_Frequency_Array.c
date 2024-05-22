a07a8dc9a55620a7b9ee04034891e193b90319c0
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-05-22 23:00:02 +0600

    s

--- Semister 01/Week 03/Module 11.5/V_Frequency_Array.c
+++ Semister 01/Week 03/Module 11.5/V_Frequency_Array.c
@@ -1,6 +1,36 @@
 // Created by M.Mahadi on 2024-05-22 22:43:34
 #include <stdio.h>
 int main(){
+    int N,M;
+    scanf("%d %d",&N,&M);
+    int arry[N+1];
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d",&arry[i]);
+    }
+    int count[M+1];
+    for (int i = 0; i <= M; i++)
+    {
+        count[i]=0;
+    }
+    for (int i = 0; i < N; i++)
+    {
+        if (arry[i]>=1 && arry[i]<=M)
+        {
+            count[arry[i]]++;
+        }
+        
+    }
+    for (int i = 0; i <= M; i++)
+    {
+        if (count[i]!=0)
+        {
+            printf("%d\n",count[i]);
+        }
+        
+    }
+    
     
     return 0;
 }
+
