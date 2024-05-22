7f8a75ab510fc7a11c589f4f2acd9f1ecf2f1521
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-05-23 01:43:52 +0600

    add

--- Semister 01/Week 04/Module 14.5/P_Shape_1.c
+++ Semister 01/Week 04/Module 14.5/P_Shape_1.c
@@ -3,15 +3,18 @@
 int main(){
     int n;
     scanf("%d",&n);
-    int k=n;
-    for (int i = 1; i <= n; i++)
+    int k = n;
+    for (int i = 0; i < n; i++)
     {
-        for (int j =1; j <= k; j++,k--)
+        for (int x = 1; x <= k; x++)
         {
             printf("*");
         }
         printf("\n");
+        k--;
     }
+    
+    
     
     return 0;
 }
---
+++ Semister 01/Week 04/Module 14.5/T_Shape_2.c
@@ -0,0 +1,16 @@
+// Created by M.Mahadi on 2024-05-23 01:42:36
+#include <stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    for (int i = 0; i < n; i++)
+    {
+       for (int i = 0; i < count; i++)
+       {
+        /* code */
+       }
+       
+    }
+    
+    return 0;
+}
