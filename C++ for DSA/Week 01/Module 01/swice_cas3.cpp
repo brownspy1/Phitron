b30529c8abbd1360cce4878fefdd61909ba64dfe
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-05-24 02:28:30 +0600

    add

---
+++ C++ for DSA/Week 01/Module 01/swice_cas3.cpp
@@ -0,0 +1,30 @@
+// Created by M.Mahadi on 2024-05-24 02:24:40
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+    char x;
+    cin >> x;
+    switch (x)
+    {
+    case 'a':
+        cout << "Vowel";
+        break;
+    case 'e':
+        cout << "Vowel";
+        break;
+    case 'i':
+        cout << "Vowel";
+        break;
+    case 'o':
+        cout << "Vowel";
+        break;
+    case 'u':
+        cout << "Vowel";
+        break;
+    default:
+        cout << "Consonant";
+    }
+    return 0;
+}
