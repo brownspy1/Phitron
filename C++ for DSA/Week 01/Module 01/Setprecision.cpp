bfaff686eb93a269d13a15a97d1dfa2cdc1fc167
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-05-24 01:25:01 +0600

    add

---
+++ C++ for DSA/Week 01/Module 01/Setprecision.cpp
@@ -0,0 +1,11 @@
+// Created by M.Mahadi on 2024-05-24 01:20:06
+#include <iostream>
+#include <stdio.h>
+#include <iomanip>
+using namespace std;
+int main(){
+    float a;
+    cin>>a;
+    cout <<fixed<<setprecision(2)<<a<<endl;
+    return 0;
+}
