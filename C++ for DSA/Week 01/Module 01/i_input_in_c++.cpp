bedb5bef92b2516de0780389579131aa1cf9e49e
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-05-24 00:55:45 +0600

    ad

---
+++ C++ for DSA/Week 01/Module 01/i_input_in_c++.cpp
@@ -0,0 +1,14 @@
+// Created by M.Mahadi on 2024-05-24 00:12:33
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main(){
+    // int a;
+    // cin>>a;
+    // cout<<a;
+    char a,b;
+    cin>>a>>b;
+    int c = a;
+    cout<<char(c);
+    return 0;
+}
