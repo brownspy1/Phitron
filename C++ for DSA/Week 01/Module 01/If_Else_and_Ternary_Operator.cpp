29286a99e778edee14b199d8e678cf6f118f4683
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-05-24 01:49:30 +0600

    add

---
+++ C++ for DSA/Week 01/Module 01/If_Else_and_Ternary_Operator.cpp
@@ -0,0 +1,18 @@
+// Created by M.Mahadi on 2024-05-24 01:26:38
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main(){
+    int a;
+    cin>>a;
+    // if (a%2==0)
+    // {
+    //     cout<<"even";
+    // }else
+    // {
+    //     cout<<"odd";
+    // }
+    (a%2==0)? cout<<"even":cout<<"odd"; // tarnary oparetor
+    
+    return 0;
+}
