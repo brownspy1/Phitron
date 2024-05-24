1f24b4944fa3fd616f6e8ca8c65c93f26ded12fd
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-05-25 01:21:15 +0600

    add

--- C++ for DSA/Week 01/Module 03/Class_and_Object.cpp
+++ C++ for DSA/Week 01/Module 03/Class_and_Object.cpp
@@ -1,9 +1,19 @@
-// Created by M.Mahadi on 2024-05-24 21:40:35
+// Created by M.Mahadi on 2024-05-25 01:18:03
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
+class student
+{
+public:
+    char name[100];
+    int roll;
+    char Secson;
+    char department;
+    long long int ragistrason;
+};
 int main()
 {
+    student cmt;
 
     return 0;
 }
