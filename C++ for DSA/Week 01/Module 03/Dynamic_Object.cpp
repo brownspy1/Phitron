9893f1b5f7c25001bbddb64af89165265e51aff1
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-05-25 12:17:26 +0600

    add

---
+++ C++ for DSA/Week 01/Module 03/Dynamic_Object.cpp
@@ -0,0 +1,32 @@
+// Created by M.Mahadi on 2024-05-25 11:52:41
+#include <iostream>
+#include <iomanip>
+using namespace std;
+class Student
+{
+public:
+    int roll;
+    int cls;
+    double cgpa;
+    Student(int roll, int cls, double cgpa)
+    {
+        this->roll = roll;
+        this->cls = cls;
+        this->cgpa = cgpa;
+    }
+};
+Student *fun()
+{
+    Student *mahadi = new Student(743678, 12, 4.00);
+
+    return mahadi;
+}
+int main()
+{
+    Student *usaR = fun();
+    cout << endl
+         << usaR->roll << endl
+         << usaR->cls << endl
+         << fixed << setprecision(2) << usaR->cgpa;
+    return 0;
+}
