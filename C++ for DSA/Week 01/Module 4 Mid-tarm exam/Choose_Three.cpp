c1463f66abe4b697bd7a34b2de465f68631f9ca4
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-05-25 23:45:21 +0600

    add

---
+++ C++ for DSA/Week 01/Module 4 Mid-tarm exam/Choose_Three.cpp
@@ -0,0 +1,42 @@
+// Created by M.Mahadi on 2024-05-25 23:29:43
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int t, m;
+        cin >> t >> m;
+        int arry[t];
+        for (int x = 0; x < t; x++)
+        {
+            cin >> arry[x];
+        }
+        bool result = false;
+        for (int a = 0; a < t - 2; a++)
+        {
+            for (int b = a + 1; b < t - 1; b++)
+            {
+                for (int c = b + 1; c < t; c++)
+                {
+                    if (arry[a] + arry[b] + arry[c] == m)
+                    {
+                        result = true;
+                    }
+                }
+            }
+        }
+        if (result)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+}
