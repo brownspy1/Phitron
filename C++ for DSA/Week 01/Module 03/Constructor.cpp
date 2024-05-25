42bfa7f84ab2244653f68144059435bdc1ec8657
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-05-25 11:15:09 +0600

    add

---
+++ C++ for DSA/Week 01/Module 03/Constructor.cpp
@@ -0,0 +1,23 @@
+// Created by M.Mahadi on 2024-05-25 10:36:37
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+class Student
+{
+public:
+    char name[100];
+    int roll;
+    char season[10];
+    Student(char name[], int roll, char season)
+    {
+
+        name = name;
+        roll = roll;
+        season = season;
+    }
+};
+int main()
+{
+
+    return 0;
+}
