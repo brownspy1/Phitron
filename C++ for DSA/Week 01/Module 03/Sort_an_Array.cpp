e49b8b204fe9f047d4ff988bc7a563839ce83a85
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-05-25 13:20:19 +0600

    add

--- C++ for DSA/Week 01/Module 03/Sort_an_Array.cpp
+++ C++ for DSA/Week 01/Module 03/Sort_an_Array.cpp
@@ -1,8 +1,18 @@
 // Created by M.Mahadi on 2024-05-25 12:26:25
 #include <iostream>
+#include <algorithm>
 using namespace std;
 int main()
 {
+    int n;
+    cin >> n;
+    int arry[n];
+    for (int i = 0; i < n; i++)
+        cin >> arry[i];
+    // sort(arry, arry + n);
+    sort(arry, arry + n, greater<int>());
+    for (int i = 0; i < n; i++)
+        cout << arry[i] << " ";
 
     return 0;
 }
