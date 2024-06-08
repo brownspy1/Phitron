ee10eae64dda4174566c22efac5d4af2043d6151
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-06-08 17:41:38 +0600

    add

---
+++ C++ for DSA/Week 03/Iterator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string::iterator it;
+    string n;
+    cin >> n;
+    cout << *n.begin() << endl;
+    cout << *n.end() << endl;
+    for (it = n.begin(); it < n.end(); it++)
+    {
+        cout << *it << endl;
+    }
+
+    return 0;
+}
