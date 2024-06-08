489d72bfef61e63b3535c656cb9b72a494ab466f
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-06-08 15:42:58 +0600

    add

--- C++ for DSA/Week 03/String_Capacity_Functions.cpp
+++ C++ for DSA/Week 03/String_Capacity_Functions.cpp
@@ -3,6 +3,18 @@
 using namespace std;
 int main()
 {
+    string n = "mahadi";
+    cout << n.size() << endl;
+    n.resize(10, 'M');
+    cout << n << endl;
+    n.clear();
+    cout << n.size() << endl;
+    cout << n.max_size() << endl;
+    cout << n.capacity() << endl;
+    if (n.empty() == true)
+        cout << "empty";
+    else
+        cout << "not empty";
 
     return 0;
 }
