6877b628d7a277c6ebd63235926bf213a52dfdf6
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-06-08 16:37:35 +0600

    add

---
+++ C++ for DSA/Week 03/Modifiers .cpp
@@ -0,0 +1,22 @@
+// Created by M.Mahadi on 2024-06-08 16:16:33
+#include <iostream>
+using namespace std;
+int main()
+{
+    string n = "HelloWorld";
+    string a = "X";
+    // n += a;
+    // n += "F";
+    cout << n.append("DD") << endl;
+    n.pop_back();
+    n.push_back('A');
+    cout << n << endl;
+    n = "Hay this is new assain string";
+    n.erase(8, 3);
+    n.replace(10, "mahadi");
+    cout << n << endl;
+    n.assign("Mahadi");
+    cout << n << endl;
+
+    return 0;
+}
