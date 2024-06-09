6edf0f0084750e989a1e84a908a125251f11d503
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-06-09 08:26:15 +0600

    add

---
+++ C++ for DSA/Week 03/Module 06/String_Constructor.cpp
@@ -0,0 +1,14 @@
+// Created by M.Mahadi on 2024-06-09 08:18:57
+#include <iostream>
+using namespace std;
+int main()
+{
+    // string n = " mahadi";
+    // string n(" mahadi");
+    // string n("I love me!", 6);
+    // string text = " Hay hello mahadi what are you doing now!";
+    // string n(text, 10);
+    string n(100000, 'M');
+    cout << n << endl;
+    return 0;
+}
