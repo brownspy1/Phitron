60dbef1d2ac87a4647da379747071dd9dd3de76d
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-06-09 16:49:15 +0600

    add

--- C++ for DSA/Week 03/Module 06/Range_Based_For_Loop.cpp
+++ C++ for DSA/Week 03/Module 06/Range_Based_For_Loop.cpp
@@ -3,6 +3,15 @@
 using namespace std;
 int main()
 {
-
+    string n;
+    cin >> n;
+    // for (int i = 0; i < n.size(); i++)
+    // {
+    //     cout << n[i] << endl;
+    // }
+    for (char X : n)
+    {
+        cout << X << endl;
+    }
     return 0;
 }
