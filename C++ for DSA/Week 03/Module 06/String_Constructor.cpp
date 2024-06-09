0da3ad4db6ea695d8061ab2792ca56eabd6ece31
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-06-09 08:28:11 +0600

    add

--- C++ for DSA/Week 03/Module 06/String_Constructor.cpp
+++ C++ for DSA/Week 03/Module 06/String_Constructor.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 int main()
 {
-    // string n = " mahadi";
-    // string n(" mahadi");
-    // string n("I love me!", 6);
-    // string text = " Hay hello mahadi what are you doing now!";
-    // string n(text, 10);
-    string n(100000, 'M');
+    string n = " mahadi";      // normal string
+    string n(" mahadi");       // normal string
+    string n("I love me!", 6); // get string {6} char I love
+    string text = " Hay hello mahadi what are you doing now!";
+    string n(text, 10); // remove 10 character from font
+    string n(10, 'M');  // print 10 M in consol
     cout << n << endl;
     return 0;
 }
