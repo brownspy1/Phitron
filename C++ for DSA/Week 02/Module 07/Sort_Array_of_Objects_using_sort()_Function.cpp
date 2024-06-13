9ce53b4b5de9c6296d53712fa0475730cd7014a1
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-06-13 18:35:17 +0600

    add

--- C++ for DSA/Week 02/Module 07/Sort_Array_of_Objects_using_sort()_Function.cpp
+++ C++ for DSA/Week 02/Module 07/Sort_Array_of_Objects_using_sort()_Function.cpp
@@ -1,8 +1,38 @@
-// Created by M.Mahadi on 2024-06-13 16:15:38
+// Created by M.Mahadi on 2024-06-13 18:35:08
 #include <iostream>
+#include <string.h>
+#include <algorithm>
 using namespace std;
+class student
+{
+public:
+    string name;
+    int roll;
+};
+bool validation(student a, student b)
+{
+    if (a.roll <= b.roll)
+    {
+        swap(a.roll, b.roll);
+    }
+}
 int main()
 {
+    int n;
+    cin >> n;
+    student list[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin.ignore();
+        getline(cin, list[i].name);
+        cin >> list[i].roll;
+    }
+
+    sort(list, list + n, validation);
+    for (int i = 0; i < n; i++)
+    {
+        cout << list[i].name << " " << list[i].roll << endl;
+    }
 
     return 0;
 }
