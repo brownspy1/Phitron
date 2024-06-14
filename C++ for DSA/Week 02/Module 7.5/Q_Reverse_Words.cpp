033684982046bc71a90fef76ee3f0632c758c6b5
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-06-15 01:38:00 +0600

    add

--- C++ for DSA/Week 02/Module 7.5/Q_Reverse_Words.cpp
+++ C++ for DSA/Week 02/Module 7.5/Q_Reverse_Words.cpp
@@ -1,8 +1,36 @@
 // Created by M.Mahadi on 2024-06-15 00:54:36
 #include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <string.h>
 using namespace std;
+void swp(string &tx)
+{
+    int n = tx.length();
+    for (int i = 0; i < n / 2; i++)
+    {
+        char temp = tx[i];
+        tx[i] = tx[n - i - 1];
+        tx[n - i - 1] = temp;
+    }
+}
 int main()
 {
+    string text;
+    getline(cin, text);
+    stringstream ss(text);
+    string word;
+    bool first_word = true;
+    while (ss >> word)
+    {
+        if (!first_word)
+        {
+            cout << " ";
+        }
+        swp(word);
+        cout << word;
+        first_word = false;
+    }
 
     return 0;
 }
