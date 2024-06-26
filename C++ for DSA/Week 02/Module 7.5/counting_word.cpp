fe0de9b777c73dbad3140d5360e3f49e0af1a1e9
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-06-27 00:31:14 +0600

    add

---
+++ C++ for DSA/Week 02/Module 7.5/counting_word.cpp
@@ -0,0 +1,7 @@
+// Created by M.Mahadi on 2024-06-27 00:30:25
+#include <iostream>
+using namespace std;
+int main(){
+    
+    return 0;
+}
--- C++ for DSA/Week 02/Module 7.5/reverse_the_array .cpp
+++ C++ for DSA/Week 02/Module 7.5/reverse_the_array .cpp
@@ -1,8 +1,39 @@
 // Created by M.Mahadi on 2024-06-26 22:02:24
 #include <iostream>
+#include <utility>
 using namespace std;
+class Student
+{
+public:
+    string name;
+    int roll;
+    int Marks;
+};
 int main()
 {
+    int n;
+    cin >> n;
+    Student ary[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> ary[i].name >> ary[i].roll >> ary[i].Marks;
+    }
+    for (int i = 0; i < n/2; i++)
+    {
+       
+            Student temp = ary[i];
+            ary[i]=ary[n-i-1];
+            ary[n-i-1]=temp;
+            // swap(ary[i],ary[j]);
+
+        
+        
+    }
+    
+    for (int i = 0; i < n; i++)
+    {
+        cout << ary[i].name << " " << ary[i].roll <<" "<< ary[i].Marks<<endl;
+    }
 
     return 0;
 }
