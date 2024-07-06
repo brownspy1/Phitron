05b571af7ec9ca1de0159f6bf8787619bb245fdd
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-07-06 17:33:39 +0600

    add

--- Semister 02/Week 01/Module 02/Vector_Initialization.cpp
+++ Semister 02/Week 01/Module 02/Vector_Initialization.cpp
@@ -1,7 +1,15 @@
 // Created by M.Mahadi on 2024-07-06 16:43:08
 #include <iostream>
+#include <vector>
 using namespace std;
 int main(){
+    vector <int> v={10,20,50};
+    vector <int> v2={1,2,5};
+    v=v2;
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout<<v[i]<<" ";
+    }
     
     return 0;
 }
