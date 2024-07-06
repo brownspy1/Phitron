56e7690517da4d4252a8dcb054632d0fc4693454
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-07-06 19:07:20 +0600

    add

--- Semister 02/Week 01/Module 02/Vector_Initialization.cpp
+++ Semister 02/Week 01/Module 02/Vector_Initialization.cpp
@@ -4,8 +4,13 @@
 using namespace std;
 int main(){
     vector <int> v={10,20,50};
-    vector <int> v2={1,2,5};
-    v=v2;
+    // vector <int> v2={1,2,5};
+    // v=v2;
+    // for (int i = 0; i < v.size(); i++)
+    // {
+    //     cout<<v[i]<<" ";
+    // }
+    v.pop_back();
     for (int i = 0; i < v.size(); i++)
     {
         cout<<v[i]<<" ";
