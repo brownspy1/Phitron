4f171e1da87d015dd277d8a661b98bb2a92aaab0
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-07-07 22:10:43 +0600

    add

---
+++ Semister 02/Week 01/Module 2.5/D_Counting_Elements.cpp
@@ -0,0 +1,12 @@
+// Created by M.Mahadi on 2024-07-07 22:09:40
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    vector <int> v(n);
+    int count = 0;
+    
+    return 0;
+}
