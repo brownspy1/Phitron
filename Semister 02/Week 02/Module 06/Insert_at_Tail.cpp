7d29f9bf1f0e5cba892658f66519730dd112865c
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-07-13 20:17:17 +0600

    add

---
+++ Semister 02/Week 02/Module 06/Insert_at_Tail.cpp
@@ -0,0 +1,34 @@
+// Created by M.Mahadi on 2024/07/13 Time 20:10:26
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+    int value;
+    Node *next;
+
+    Node(int value)
+    {
+        this->value = value;
+        this->next = NULL;
+    }
+};
+void insart_tail(Node *head, int value)
+{
+    if (head == NULL)
+    {
+        /* code */
+    }
+    
+}
+int main()
+{
+    Node
+    cout << "Choose your option" << endl
+         << "1: Insart in tail" << endl
+         << "2.Print Linklist" << endl
+         << "3.Exit" << endl
+         << "Enter:";
+        
+    return 0;
+}
--- Semister 02/Week 02/Module 06/Reference_of_a_Pointer.cpp
+++ Semister 02/Week 02/Module 06/Reference_of_a_Pointer.cpp
@@ -1,13 +1,14 @@
 // Created by M.Mahadi on 2024/07/13 Time 18:03:44
 #include <iostream>
 using namespace std;
-void fun(int * p){
+void fun(int * &p){
     *p = NULL;
+    cout<<*p<<endl;
 }
 int main(){
     int x = 50;
     int * m = &x;
     fun(m);
-    cout<<*m;
+    cout<<*m<<endl;
     return 0;
 }
