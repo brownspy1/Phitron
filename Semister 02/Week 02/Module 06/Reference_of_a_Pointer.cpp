0aaa42911767dfe406f8c3bb8058cdac73873b03
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-07-13 18:04:48 +0600

    add

---
+++ Semister 02/Week 02/Module 06/Reference_of_a_Pointer.cpp
@@ -0,0 +1,17 @@
+// Created by M.Mahadi on 2024/07/13 Time 18:03:44
+#include <iostream>
+using namespace std;
+class NODE {
+    public:
+    int value;
+    NODE * next;
+    
+    NODE(int value){
+        this->value = value;
+        this->next =NULL;
+    }
+};
+int main(){
+    
+    return 0;
+}
