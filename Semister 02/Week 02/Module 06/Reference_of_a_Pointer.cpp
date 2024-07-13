9ff84e7f000edeac3e2b378c9f437e20e0788679
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-07-13 18:07:40 +0600

    add

--- Semister 02/Week 02/Module 06/Reference_of_a_Pointer.cpp
+++ Semister 02/Week 02/Module 06/Reference_of_a_Pointer.cpp
@@ -1,17 +1,13 @@
 // Created by M.Mahadi on 2024/07/13 Time 18:03:44
 #include <iostream>
 using namespace std;
-class NODE {
-    public:
-    int value;
-    NODE * next;
-    
-    NODE(int value){
-        this->value = value;
-        this->next =NULL;
-    }
-};
+void fun(int * p){
+    *p = NULL;
+}
 int main(){
-    
+    int x = 50;
+    int * m = &x;
+    fun(m);
+    cout<<*m;
     return 0;
 }
