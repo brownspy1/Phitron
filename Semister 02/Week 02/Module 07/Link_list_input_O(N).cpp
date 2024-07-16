cab16ecf93b4ef5060d20c96b221862559042ae9
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-07-16 18:58:34 +0600

    add

--- Semister 02/Week 02/Module 07/Link_list_input_O(N).cpp
+++ Semister 02/Week 02/Module 07/Link_list_input_O(N).cpp
@@ -13,12 +13,111 @@ public:
         this->next = NULL;
     }
 };
+void Insart_all(Node*&Head,int value){
+    Node * newNode = new Node(value);
+    
+    if (Head == NULL)
+    {
+        Head = newNode;
+        return;    
+    }
+    Node * temp = Head;
+    while (temp->next!=NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+
+}
+void insert_tail(Node *&Head,Node *&tail,int value){
+    Node *newNode = new Node(value);
+    if (Head == NULL)
+    {
+        Head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    tail = newNode;
+    
+}
+void print(Node *Head){
+    Node * Temp = Head;
+    while (Temp!=NULL)
+    {
+        cout<<Temp->value<<" ";
+        Temp = Temp->next;
+    }
+    cout<<endl;
+}
+int count(Node *Head){
+    int count = 0;
+    Node *temp = Head;
+    while (temp!=NULL)
+    {
+        count++;
+        temp = temp->next;
+    }
+    return count;
+}
+void Deleted(Node*&Head,int position){ 
+    if (position >= count(Head) || Head == NULL)
+    {
+        cout<<"Invalid Position"<<endl;
+    }else
+    {
+        if (position == 0)
+        {
+            Node* Deleted = Head;
+            Head = Head->next;
+            delete Deleted; 
+        }else
+        {
+            Node * temp = Head;
+            for (int i = 1; i < position; i++)
+            {
+                temp = temp->next;
+            }
+            Node*Deleted = temp->next;
+            temp->next = temp->next->next;
+            delete Deleted;
+        }
+        
+        
+    }
+    
+    
+
+}
 int main(){
+    Node *head = NULL;
+    Node *tail = NULL;
     while (true)
     {
 
-        cout<<endl<<"1.Insart all"<<endl<<"2.Print all"<<endl<<"3.Delete last";
+        int option;
+        cout<<endl
+        <<"1.Insart all"<<endl
+        <<"2.Print all"<<endl
+        <<"3.Insert Tail"<<endl
+        <<"4.Delete last"<<endl
+        <<"Enter:";
+        cin>>option;
+        switch (option)
+        {
+            case 1:
+                int value;
+                while (true)
+                {
+                    cin>>value;
+                    if(value == -1) break;
+                    Insart_all(head,value);
+                }
+                break;
+            case 2:
+                
+        }
+        
     }
-    
     return 0;
 }
