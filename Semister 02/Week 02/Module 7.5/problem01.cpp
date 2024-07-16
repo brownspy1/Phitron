706d56b6edfa7a4295a8707cfddc3445a520c335
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-07-16 20:38:48 +0600

    add

---
+++ Semister 02/Week 02/Module 7.5/problem01.cpp
@@ -0,0 +1,19 @@
+// Created by M.Mahadi on 2024-07-16 Time:20-35-35
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+    int value;
+    Node *next;
+
+    Node(int value)
+    {
+        this->value = value;
+        this->next = NULL;
+    }
+};
+int main(){
+    
+    return 0;
+}
