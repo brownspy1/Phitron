11a254c79158864617e07bceee4fb0ec70a9b7a3
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-07-17 21:16:15 +0600

    working

--- Semister 02/Week 02/Assainment 02/Remove_Duplicate.cpp
+++ Semister 02/Week 02/Assainment 02/Remove_Duplicate.cpp
@@ -13,8 +13,46 @@ public:
         this->next = NULL;
     }
 };
+void input(Node *&Head, int value, Node *&Tail)
+{
+    Node *NewNode = new Node(value);
+    if (Head == NULL)
+    {
+        Head = NewNode;
+        Tail = NewNode;
+        return;
+    }
+
+    Tail->next = NewNode;
+    Tail = NewNode;
+}
+void print(Node *head)
+{
+    Node *Temp = head;
+    for (Node *i = Temp; i != NULL; i = i->next)
+    {
+        cout << i->value << " ";
+    }
+}
+void remove_duplicat(Node *Head)
+{
+    Node *Temp = Head;
+    for (Node *i = Temp; i != NULL; i = i->next)
+    {
+        /* code */
+    }
+}
 int main()
 {
-
+    Node *Head = NULL;
+    Node *Tail = NULL;
+    int value;
+    while (true)
+    {
+        cin >> value;
+        if (value == -1)
+            break;
+        input(Head, value, Tail);
+    }
     return 0;
 }
