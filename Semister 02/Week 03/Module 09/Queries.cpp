52fb1bd6e07f7743b54b5807f7528df1349c132a
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-07-18 13:48:37 +0600

    Done

---
+++ Semister 02/Week 03/Module 09/Queries.cpp
@@ -0,0 +1,20 @@
+// Created by M.Mahadi on 2024-07-18 Time:13-48-20
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+    int value;
+    Node *next;
+
+    Node(int value)
+    {
+        this->value = value;
+        this->next = NULL;
+    }
+};
+int main()
+{
+
+    return 0;
+}
