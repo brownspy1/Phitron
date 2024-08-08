4a5d91d40b6cf921c6ec62605b24a43ab8ac4efe
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-08-09 02:57:35 +0600

    add

--- Semister 01/Week 05/Module 18/Secondary_Diagonal_Matrix.c
+++ Semister 01/Week 05/Module 18/Secondary_Diagonal_Matrix.c
@@ -1,6 +1,35 @@
 // Created by M.Mahadi on 2024-08-08 Time:23:30:45
 #include <stdio.h>
+#include <stdbool.h>
 int main(){
+    int rowe,colume;scanf("%d %d",&rowe,&colume);
+    int array[rowe][colume];
+    for (int i = 0; i < rowe; i++)
+    {
+        for (int j = 0; j < colume; j++)
+        {
+            scanf("%d",&array[i][j]);
+        }
+        
+    }
     
+    bool flag = true;
+    if (rowe != colume)
+    {
+        flag = false;
+    }
+    
+    for (int i = 0; i < rowe; i++)
+    {
+        for (int j = 0; j < colume; j++)
+        {
+            if (array[i][j]!=0)
+            {
+                flag = false;
+            }
+            
+        }
+        
+    }
     return 0;
 }
