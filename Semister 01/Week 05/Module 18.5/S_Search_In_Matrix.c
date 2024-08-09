b24235b9494de3043de2beb107eeb2514942b80d
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-08-09 19:37:52 +0600

    Done

--- Semister 01/Week 05/Module 18.5/S_Search_In_Matrix.c
+++ Semister 01/Week 05/Module 18.5/S_Search_In_Matrix.c
@@ -1,5 +1,6 @@
 // Created by M.Mahadi on 2024-08-09 Time:19:20:27
 #include <stdio.h>
+#include <stdbool.h>
 int main()
 {
     int n, m;
@@ -12,6 +13,28 @@ int main()
         {
             scanf("%d", &array[i][j]);
         }
+    }
+
+    int search;
+    scanf("%d", &search);
+    bool flag = false;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (array[i][j] == search)
+            {
+                flag = true;
+            }
+        }
+    }
+    if (flag)
+    {
+        printf("will not take number");
+    }
+    else
+    {
+        printf("will take number");
     }
 
     return 0;
