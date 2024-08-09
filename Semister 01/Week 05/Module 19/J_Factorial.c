a1f50e458cd10b0b3c168f0abd5505cccf24b3b4
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-08-09 22:52:23 +0600

    add

--- Semister 01/Week 05/Module 19/J_Factorial.c
+++ Semister 01/Week 05/Module 19/J_Factorial.c
@@ -1,10 +1,17 @@
 // Created by M.Mahadi on 2024-08-09 Time:22:12:20
 #include <stdio.h>
-int facet()
+long long int facet(long long int n)
 {
+    if (n == 0)
+        return 1;
+    long long int val = facet(n - 1);
+    return n * val;
 }
 int main()
 {
-
+    long long int n;
+    scanf("%lld", &n);
+    long long int ans = facet(n);
+    printf("%lld", ans);
     return 0;
 }
