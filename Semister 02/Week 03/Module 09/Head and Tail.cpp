120fded13e42f335eb01db5e434f5b1a7a68b323
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-08-12 01:57:44 +0600

    add

--- Semister 02/Week 03/Module 09/Head and Tail.cpp
+++ Semister 02/Week 03/Module 09/Head and Tail.cpp
@@ -49,20 +49,33 @@ void insert_at_head(Node *&Head, Node *&Tail, int Value)
 void insert_at_tail(Node *&Head, Node *&Tail, int value)
 {
     Node *NewNode = new Node(value);
-    if (Head != NULL)
+    if (Head == NULL)
     {
         insert_at_head(Head, Tail, value);
         return;
     }
     Tail->next = NewNode;
     NewNode->prev = Tail;
+    Tail = NewNode;
 };
 int main()
 {
     Node *Head = NULL;
     Node *Tail = NULL;
-    int value, position;
-    cin >> value, position;
+    while (true)
+    {
+        int value;
+        cin >> value;
+        if (value == -1)
+        {
+            break;
+        }
+        insert_at_tail(Head, Tail, value);
+    }
+
+    // int value, position;
+    // cin >> value, position;
+    // insert_at_tail(Head, Tail, value);
     print(Head);
     reverse(Tail);
     return 0;
