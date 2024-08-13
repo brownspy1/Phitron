5b7362a0d3d9893ec96cf457118ab0bc57af7319
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-08-13 16:40:28 +0600

    add

--- Semister 02/Week 03/Module 10/STL_Modifiers.cpp
+++ Semister 02/Week 03/Module 10/STL_Modifiers.cpp
@@ -1,8 +1,27 @@
 // Created by M.Mahadi on 2024-08-13 Time:15-48-15
 #include <iostream>
+#include <list>
 using namespace std;
 int main()
 {
+    int ar[9] = {10, 20, 30, 40, 50, 60, 70, 80, 90};
+    list<int> myList(ar, ar + 9);
+    list<int> myList2;
+    myList.push_front(100);
+    myList.pop_front();
+    myList.insert(next(myList.begin(), 2), 500);
+    myList.insert(next(myList.begin(), 2), {1, 2, 3, 4, 5, 6});
+    myList.insert(next(myList.begin(), 4), {500, 600, 700});
+    myList.erase(next(myList.begin(), 2), myList.end()); // if i uzse end() delet all from my pointer
+    // myList2.assign(myList.begin(), myList.end());
+    // myList2.push_back(100);
+    // myList2.push_back(110);
+    // myList.pop_back();
+    myList2 = myList;
+    for (auto &&i : myList2)
+    {
+        cout << i << endl;
+    }
 
     return 0;
 }
---
+++ Semister 02/Week 03/Module 10/a.cpp
@@ -0,0 +1,8 @@
+// Created by M.Mahadi on 2024-08-13 Time:16-37-07
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "hello worl";
+    return 0;
+}
