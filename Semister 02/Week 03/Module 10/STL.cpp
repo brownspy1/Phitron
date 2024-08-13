d5e6c4c2b6acdc3621ccbe4cdf886b5e20c81b3b
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-08-13 15:01:43 +0600

    add

--- Semister 02/Week 03/Module 10/STL.cpp
+++ Semister 02/Week 03/Module 10/STL.cpp
@@ -5,19 +5,22 @@ using namespace std;
 int main()
 {
     // list<int> myList(10); // size with gavage value
-    list<int> myList(10, 50); // size with  value
+    int l[10] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+    list<int> myList(10, 50);     // size with  value
+    list<int> myList2(l, l + 10); // copy from int array of l
 
-    // itaretor of this STL
-    for (auto i = myList.begin(); i != myList.end(); i++)
-    {
-        cout << *i << endl;
-    }
-
-    // // range base loop
-    // for (auto i : myList)
+    // iterator of this STL
+    // list<int>::iterator i; // aita old new aitar pori borta auto use kora
+    // for (auto i = myList.begin(); i != myList.end(); i++)
     // {
-    //     cout << i << endl;
+    //     cout << *i << endl;
     // }
+
+    // range base loop
+    for (auto i : myList2)
+    {
+        cout << i << endl;
+    }
 
     return 0;
 }
