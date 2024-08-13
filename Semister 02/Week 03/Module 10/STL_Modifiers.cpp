c80b0049962771bd866dae04057a970ddf8359ba
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-08-13 16:57:54 +0600

    add

--- Semister 02/Week 03/Module 10/STL_Modifiers.cpp
+++ Semister 02/Week 03/Module 10/STL_Modifiers.cpp
@@ -1,6 +1,7 @@
 // Created by M.Mahadi on 2024-08-13 Time:15-48-15
 #include <iostream>
 #include <list>
+#include <algorithm>
 using namespace std;
 int main()
 {
@@ -12,7 +13,9 @@ int main()
     myList.insert(next(myList.begin(), 2), 500);
     myList.insert(next(myList.begin(), 2), {1, 2, 3, 4, 5, 6});
     myList.insert(next(myList.begin(), 4), {500, 600, 700});
-    myList.erase(next(myList.begin(), 2), myList.end()); // if i uzse end() delet all from my pointer
+    myList.erase(next(myList.begin(), 2), next(myList.begin(), 8)); // if i uzse end() delet all from my pointer
+    replace(myList.begin(), myList.end(), 500, 100);
+    find(myList.begin(), myList.end(), );
     // myList2.assign(myList.begin(), myList.end());
     // myList2.push_back(100);
     // myList2.push_back(110);
