6fc9c9f9399f53cbf97fd1a64a8ba9a547a31469
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-08-18 20:18:43 +0600

    Done✔

---
+++ Semister 02/Week 04/Module 13/STL Stack.cpp
@@ -0,0 +1,8 @@
+// Created by M.Mahadi on 2024-08-18 Time:20-18-34
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    return 0;
+}
--- Semister 02/Week 04/Module 13/Stack Implement using Doubly Linked List.cpp
+++ Semister 02/Week 04/Module 13/Stack Implement using Doubly Linked List.cpp
@@ -19,11 +19,72 @@ class myStack
 private:
     Node *Head = NULL;
     Node *Tail = NULL;
+    int count = 0;
 
 public:
+    void push(int value)
+    {
+        Node *newNode = new Node(value);
+        count++;
+        if (Head == NULL)
+        {
+            Head = newNode;
+            Tail = newNode;
+            return;
+        }
+        Tail->next = newNode;
+        newNode->prev = Tail;
+        Tail = newNode;
+    }
+    int top()
+    {
+        return Tail->value;
+    }
+    void pop()
+    {
+        count--;
+        Node *Deleted = Tail;
+        Tail = Tail->prev;
+        if (Tail != NULL)
+        {
+            Tail->next = NULL;
+        }
+        else
+        {
+            Head = NULL;
+        }
+
+        delete Deleted;
+    }
+    int size()
+    {
+        return count;
+    }
+    bool empty()
+    {
+        if (count == 0)
+            return true;
+        else
+            return false;
+    }
 };
 int main()
 {
+    myStack stack;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        stack.push(x);
+    }
+
+    while (!stack.empty())
+    {
+        cout << stack.top() << endl;
+        stack.pop();
+    }
 
     return 0;
 }
