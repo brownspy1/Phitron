d14804c47d0fca9c2c974861c202d78f4ae556d6
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-08-20 18:56:20 +0600

    Done✔

--- Semister 02/Week 04/Module 14.5/problem02.cpp
+++ Semister 02/Week 04/Module 14.5/problem02.cpp
@@ -1,8 +1,17 @@
-// Created by M.Mahadi on 2024-08-20 Time:16-29-04
+//*Created by M.Mahadi on 2024-08-20 Time:16-29-04
 #include <iostream>
+#include <stack>
+#include <queue>
 using namespace std;
 int main()
 {
+    stack<int> s;
+    queue<int> q;
+
+    int n;
+    cin >> n;
+    int m;
+    cin >> m;
 
     return 0;
 }
