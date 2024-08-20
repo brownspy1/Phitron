d5c743ac225d7fb89299dae01d6834d6dbf9629f
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-08-20 20:17:06 +0600

    Done✔

--- Semister 02/Week 04/Module 14.5/problem 04.cpp
+++ Semister 02/Week 04/Module 14.5/problem 04.cpp
@@ -1,6 +1,7 @@
 // Created by M.Mahadi on 2024-08-20 Time:19-51-37
 #include <iostream>
 #include <queue>
+#include <stack>
 using namespace std;
 int main()
 {
@@ -10,6 +11,27 @@ int main()
     cin >> n;
     for (int i = 0; i < n; i++)
     {
+        int x;
+        cin >> x;
+        myQueue.push(x);
+    }
+    stack<int> myStack;
+    while (!myQueue.empty())
+    {
+        int v = myQueue.front();
+        myStack.push(v);
+        myQueue.pop();
+    }
+    while (!myStack.empty())
+    {
+        int v = myStack.top();
+        myQueue2.push(v);
+        myStack.pop();
+    }
+    while (!myQueue2.empty())
+    {
+        cout << myQueue2.front() << " ";
+        myQueue2.pop();
     }
 
     return 0;
