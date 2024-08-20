2cc9d2216b49ca43d19d37374bce600c3a729eb1
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-08-20 19:05:21 +0600

    Solved✔

--- Semister 02/Week 04/Module 14.5/problem02.cpp
+++ Semister 02/Week 04/Module 14.5/problem02.cpp
@@ -10,8 +10,44 @@ int main()
 
     int n;
     cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        s.push(x);
+    }
+
     int m;
     cin >> m;
+    for (int i = 0; i < m; i++)
+    {
+        int x;
+        cin >> x;
+        q.push(x);
+    }
+    bool flag = true;
+    if (s.size() != q.size())
+    {
+        flag = false;
+    }
+
+    while (!s.empty())
+    {
+        if (s.top() != q.front())
+        {
+            flag = false;
+        }
+        s.pop();
+        q.pop();
+    }
+    if (flag)
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
 
     return 0;
 }
