b226d3c730e5dbfa8adadbd02f29c86c36e0aad6
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-08-20 20:06:34 +0600

    Add

---
+++ Semister 02/Week 04/Module 14.5/problem 04.cpp
@@ -0,0 +1,16 @@
+// Created by M.Mahadi on 2024-08-20 Time:19-51-37
+#include <iostream>
+#include <queue>
+using namespace std;
+int main()
+{
+    queue<int> myQueue;
+    queue<int> myQueue2;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+    }
+
+    return 0;
+}
