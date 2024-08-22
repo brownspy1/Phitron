59428883f06a0b39b59d242b12136fd5101f2a44
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-08-22 20:29:03 +0600

    Done✔

---
+++ Semister 02/Week 04/Assainment 3/Same_or_Not_II.cpp
@@ -0,0 +1,26 @@
+// Created by M.Mahadi on 2024-08-22 Time:19-52-27
+#include <iostream>
+#include <list>
+using namespace std;
+class Stack
+{
+private:
+    list<int> myList;
+    int cnt = 0;
+
+public:
+    void push() {}
+    void pop() {}
+    int top() {}
+    bool empty() {}
+};
+class Queue
+{
+private:
+public:
+};
+int main()
+{
+
+    return 0;
+}
