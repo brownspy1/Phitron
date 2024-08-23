0725eebd81a155627f89b05d40e4b0a0dc965a65
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-08-23 19:49:09 +0600

    Solved✔

--- Semister 02/Week 04/Assainment 3/Special_Queries.cpp
+++ Semister 02/Week 04/Assainment 3/Special_Queries.cpp
@@ -5,6 +5,39 @@
 using namespace std;
 int main()
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin >> n;
+    queue<string> st;
+    while (n--)
+    {
+        int command;
+        cin >> command;
+        if (command == 0)
+        {
+            string Name;
+            cin >> Name;
+            st.push(Name);
+        }
+        else if (command == 1)
+        {
+            if (!st.empty())
+            {
+                string user = st.front();
+                st.pop();
+                cout << user << endl;
+            }
+            else
+            {
+                cout << "Invalid" << endl;
+            }
+        }
+        else
+        {
+            cout << "Invalid" << endl;
+        }
+    }
 
     return 0;
 }
