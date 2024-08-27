5fb089c97c3bdd8060a71d5b6fe05e8479b5f24b
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-08-27 21:32:47 +0600

    Solved✔

---
+++ Semister 02/Week 05/Assignment 04/Leaf_Nodes.cpp
@@ -0,0 +1,58 @@
+// Created by M.Mahadi on 2024-08-27 Time:21-31-15
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    Node *left;
+    int value;
+    Node *right;
+
+    Node(int value)
+    {
+        this->left = NULL;
+        this->value = value;
+        this->right = NULL;
+    }
+};
+Node *input()
+{
+    Node *root = NULL;
+    int n;
+    cin >> n;
+    if (n != -1)
+    {
+        root = new Node(n);
+    }
+    queue<Node *> myQ;
+    if (root)
+        myQ.push(root);
+    while (!myQ.empty())
+    {
+        Node *parent = myQ.front();
+        myQ.pop();
+
+        Node *left = NULL;
+        Node *right = NULL;
+        int l, r;
+        cin >> l >> r;
+        if (l != -1)
+            left = new Node(l);
+        if (r != -1)
+            right = new Node(r);
+
+        parent->left = left;
+        parent->right = right;
+
+        if (parent->left)
+            myQ.push(parent->left);
+        if (parent->right)
+            myQ.push(parent->right);
+    }
+    return root;
+}
+int main()
+{
+
+    return 0;
+}
