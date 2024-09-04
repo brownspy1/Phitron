19982ebafa12e08100ded6087cc11c1e29d792fb
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-09-04 20:57:09 +0600

    Add

--- Semister 02/Week 06/Module 22/Insert in Heap Implementation.cpp
+++ Semister 02/Week 06/Module 22/Insert in Heap Implementation.cpp
@@ -1,7 +1,7 @@
 // Created by M.Mahadi on 2024-09-04 Time:20-21-30
 #include <bits/stdc++.h>
 using namespace std;
-void insert_heap(vector<int> v, int x)
+void insert_heap(vector<int> &v, int x)
 {
     v.push_back(x);
     int curent_idx = v.size() - 1;
@@ -20,9 +20,14 @@ void insert_heap(vector<int> v, int x)
 int main()
 {
     vector<int> v;
-    int x;
-    cin >> x;
-
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        insert_heap(v, x);
+    }
     for (auto &&i : v)
     {
         cout << i << " ";
