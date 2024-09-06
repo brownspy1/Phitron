996d3b3498b60ba740079bfb80a47b7eef83af03
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-09-06 14:20:18 +0600

    Deleted

---
+++ Semister 02/Week 06/Module 23/STL Priority Queue.cpp
@@ -0,0 +1,14 @@
+// Created by M.Mahadi on 2024-09-06 Time:14-15-00
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    priority_queue<int> heap;
+    int command;
+    cin >> command;
+    while (true)
+    {
+    }
+
+    return 0;
+}
