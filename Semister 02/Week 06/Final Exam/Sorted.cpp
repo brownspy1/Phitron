39ee4a829076594da138bbbeac7d071a553b4138
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-09-08 01:17:51 +0600

    Add

---
+++ Semister 02/Week 06/Final Exam/Sorted.cpp
@@ -0,0 +1,20 @@
+// Created by M.Mahadi on 2024-09-08 Time:01:12:08
+#include <bits/stdc++.h>
+using namespace std;
+#define __fast                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+int main()
+{
+    __fast
+
+        set<int>
+            num;
+    int n;
+    cin >> n;
+    while (n--)
+    {
+    }
+
+    return 0;
+}
