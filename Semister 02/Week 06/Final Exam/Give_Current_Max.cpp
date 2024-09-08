847b89e88438fe2ec078aa627405db87d4be331e
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-09-08 11:08:23 +0600

    One the way

--- Semister 02/Week 06/Final Exam/Give_Current_Max.cpp
+++ Semister 02/Week 06/Final Exam/Give_Current_Max.cpp
@@ -22,12 +22,56 @@ class cmp_max
 public:
     bool operator()(student a, student b)
     {
+        if (a.marks == b.marks)
+        {
+            return a.roll > b.roll;
+        }
+        return a.marks < b.marks;
     }
 };
 int main()
 {
     __fast;
     priority_queue<student, vector<student>, cmp_max> user;
+    int n;
+    cin >> n;
+    while (n--)
+    {
+        string name;
+        int roll, marks;
+        cin >> name >> roll >> marks;
+        student data(name, roll, marks);
+    }
+    int x;
+    cin >> x;
+    while (x--)
+    {
+        int c;
+        cin >> c;
+        if (c == 0)
+        {
+            string name;
+            int roll, marks;
+            cin >> name >> roll >> marks;
+            student data(name, roll, marks);
+            cout << user.top().name << " " << user.top().roll << " " << user.top().marks;
+        }
+        else if (c == 1)
+        {
+            if (!user.empty())
+            {
+                cout << user.top().name << " " << user.top().roll << " " << user.top().marks;
+            }
+            else
+            {
+                cout << "Empty" << endl;
+            }
+        }
+        else if (c == 2)
+        {
+            /* code */
+        }
+    }
 
     return 0;
 }
