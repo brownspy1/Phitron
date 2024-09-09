af3557ed3f8d9b462071b0ec989c45dfd356aae6
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-09-09 22:04:46 +0600

    ADJ

---
+++ Algorithms/Week 01/Module 01/Adjacency Matrix.cpp
@@ -0,0 +1,35 @@
+// Created by M.Mahadi on 2024-09-09 Time:21-49-35
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    int adj[n][n];
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < n; j++)
+    //     {
+    //         adj[i][j] = 0;
+    //     }
+    // }
+    memset(adj, 0, sizeof(adj));
+    for (int i = 0; i < n; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        adj[a][b] = 1;
+        adj[b][a] = 1;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << adj[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
