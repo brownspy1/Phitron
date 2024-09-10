bbc2da97490ef228f7739d63fdef81e4b3486fec
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-09-10 17:23:16 +0600

    Add

--- Algorithms/Week 01/Module 01/edge_list.cpp
+++ Algorithms/Week 01/Module 01/edge_list.cpp
@@ -2,6 +2,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
+    int n,e;
+    cin>>n>>e;
+    vector<pair<int,int>> v;
+    while (e--)
+    {
+        int a,b;cin>>a>>b;
+        v.push_back({a,b});
+    }
+    for (auto &&i : v)
+    {
+        cout<<i.first<<"<-->"<<i.second<<endl;
+    }
+    
     
     return 0;
 }
