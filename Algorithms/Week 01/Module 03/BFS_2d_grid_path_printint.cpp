4daa8192045e10b7b424207fc89100fd90fb92f9
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-09-13 20:51:53 +0600

    Add

--- Algorithms/Week 01/Module 03/BFS_2d_grid_path_printint.cpp
+++ Algorithms/Week 01/Module 03/BFS_2d_grid_path_printint.cpp
@@ -6,6 +6,8 @@ const int N = 12;
 char TwoD[N][N]; // stor 2d grid
 bool vis[N][N];  // for visit track
 int level[N][N]; // for level track
+pair<int,int> parent[N][N];
+
 // vector<pair<int, int>> d = {{0, 1}, {0, -1}, {-1, 0}, {1, 0},{-1,1},{-1,-1},{1,1},{1,-1}}; // dan bam upor nic uporer cornar nicer cornar
 vector<pair<int, int>> d = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}}; // dan bam upor nic
 
@@ -22,14 +24,14 @@ void BFS_travars(int si, int sj)
     q.push({si, sj});
     vis[si][sj] = true;
     level[si][sj] = 0;
+    parent[si][sj] = {-1,-1};
     while (!q.empty())
     {
         pair<int, int> par = q.front();
         q.pop();
         int a = par.first;
         int b = par.second;
-        cout<<TwoD[a][b]<<" ";
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < 8; i++)
         {
             int ci = a + d[i].first;
             int cj = b + d[i].second;
@@ -38,12 +40,25 @@ void BFS_travars(int si, int sj)
                 q.push({ci, cj});
                 vis[ci][cj] = true;
                 level[ci][cj] = level[a][b] + 1;
+                parent[ci][cj] = {a,b};
             }
         }
-        cout<<endl;
     }
 }
-
+void path_print(int si, int sj, int di, int dj)
+{
+    vector<int> pathe;
+    pair<int,int> carent = {di,dj};
+    while (carent != make_pair(-1,-1))
+    {
+        pathe.push_back(TwoD[carent.first][carent.second]);
+        carent = parent[carent.first][carent.second];
+    }
+    reverse(pathe.begin(),pathe.end());
+    for(char i:pathe){
+        cout<<"-->"<<i;
+    }
+}
 int main()
 {
     cin >> r >> c;
@@ -60,6 +75,7 @@ int main()
     memset(vis, false, sizeof(vis));
     memset(level, -1, sizeof(level));
     BFS_travars(si, sj);
-    
+    int di,dj;cin>>di>>dj;
+    path_print(si,sj,di,dj);
     return 0;
 }
