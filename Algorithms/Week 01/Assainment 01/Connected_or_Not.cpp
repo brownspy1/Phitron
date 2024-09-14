cd9fd4d7b0fd151bb98e67e5e84ad338e98f4603
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-09-14 12:01:07 +0600

    Add

--- Algorithms/Week 01/Assainment 01/Connected_or_Not.cpp
+++ Algorithms/Week 01/Assainment 01/Connected_or_Not.cpp
@@ -1,50 +1,85 @@
 // Created by M.Mahadi on 2024-09-14 Time:05-52-41
 #include <bits/stdc++.h>
 using namespace std;
-vector<pair<int, int>> eageList;
+int adj[1000][1000];
 int n, e;
 int main()
 {
-    cin>>n>>e;
+    cin >> n >> e;
+    memset(adj, 0, sizeof(adj));
     for (int i = 0; i < e; i++)
     {
-         int a, b;cin>>a>>b;
-        eageList.push_back({a,b});
+        int a, b;
+        cin >> a >> b;
+        adj[a][b] = 1;
     }
-    
-    int q;cin>>q;
+    int q;
+    cin >> q;
     while (q--)
     {
-        bool flag = false;  
-        int x,y;
-        cin>>x>>y;
-        for (int i = 0; i < e; i++)
+        int x, y;
+        cin >> x >> y;
+        if (x == y)
         {
-            if (eageList[i].first == x && eageList[i].second == y)
-            {
-                flag = true;
-                break;
-            }else if (eageList[i].first == x && eageList[i].first == y)
-            {
-                flag = true;
-                break;
-            }else if (eageList[i].second == x && eageList[i].second == y)
-            {
-                flag = true;
-                break;
-            }
-            
-            
-
+            cout << "YES" << endl;
         }
-        if (flag)
+        else if (adj[x][y] == 1)
         {
-            cout<<"YES"<<endl;
-        }else cout<<"NO"<<endl;
-
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
     }
-
-    
 
     return 0;
 }
+// // Created by M.Mahadi on 2024-09-14 Time:05-52-41
+// #include <bits/stdc++.h>
+// using namespace std;
+// vector<pair<int, int>> eageList;
+// int n, e;
+// int main()
+// {
+//     cin >> n >> e;
+//     for (int i = 0; i < e; i++)
+//     {
+//         int a, b;
+//         cin >> a >> b;
+//         eageList.push_back({a, b});
+//     }
+
+//     int q;
+//     cin >> q;
+//     while (q--)
+//     {
+//         bool flag = false;
+//         int x, y;
+//         cin >> x >> y;
+//         if (x == y)
+//         {
+//             flag = true;
+//         }
+//         else
+//         {
+//             for (int i = 0; i < e; i++)
+//             {
+//                 if (eageList[i].first == x && eageList[i].second == y)
+//                 {
+//                     flag = true;
+//                     break;
+//                 }
+//             }
+//         }
+
+//         if (flag)
+//         {
+//             cout << "YES" << endl;
+//         }
+//         else
+//             cout << "NO" << endl;
+//     }
+
+//     return 0;
+// }
