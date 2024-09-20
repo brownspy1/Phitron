e11cc12dc5b72761dfbaedb4c2854c5d50ac2891
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-09-21 00:05:20 +0600

    Done

--- Contest/Monthly contest 01/Chocolate_Chocolate.cpp
+++ Contest/Monthly contest 01/Chocolate_Chocolate.cpp
@@ -1,57 +1,33 @@
-// // Created by M.Mahadi on 2024-09-20 Time:21-42-06
-// #include <bits/stdc++.h>
-// using namespace std;
-// int chocolate(int peace,int wrap)
-// {
-//     if (peace < 3) return 0;
-
-
-        
-    
-// } 
-// int main()
-// {
-//     int t;
-//     cin >> t;
-//     while (t--)
-//     {
-//         int taka;
-//         cin >> taka;
-//         int pic = taka/5;
-//         int wrap = pic/3;
-//         cout << chocolate(pic,wrap);
-//     }
-
-//     return 0;
-// }
-
 // Created by M.Mahadi on 2024-09-20 Time:21-42-06
 #include <bits/stdc++.h>
 using namespace std;
-
-int chocolate(int peace) {
-    int total_chocolates = peace; 
+int chocolate(int peace)
+{
+    int allPack = peace; 
     int wrap = peace; 
 
     
     while (wrap >= 3) {
-        int new_chocolates = wrap / 3;
-        total_chocolates += new_chocolates; 
-        wrap = wrap % 3 + new_chocolates; 
+        int newPack = wrap / 3;
+        allPack += newPack; 
+        wrap = wrap % 3 + newPack; 
     }
 
-    return total_chocolates;
-}
+    return allPack;
 
-int main() {
+} 
+int main()
+{
     int t;
     cin >> t;
-    while (t--) {
+    while (t--)
+    {
         int taka;
         cin >> taka;
-        int pic = taka / 5; 
-        cout << chocolate(pic) << '\n'; 
+        int pic = taka/5;
+        cout << chocolate(pic)<<endl;
     }
 
     return 0;
 }
+
