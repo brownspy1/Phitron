17af4952f28db7c943a0324db7322d6c4179f4b7
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-10-02 01:38:01 +0600

    add

--- Semister 02/Algorithms/Week 05/Module 16/recarson.cpp
+++ Semister 02/Algorithms/Week 05/Module 16/recarson.cpp
@@ -2,12 +2,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 int fun(int n){
-    if (n == 10) return 0;
+    if (n == 0) return 0;
+    fun(n-1);
     cout<<n<<endl;
-    fun(n+1);
     
 }
 int main(){
-    fun(1);
+    fun(10);
     return 0;
 }
