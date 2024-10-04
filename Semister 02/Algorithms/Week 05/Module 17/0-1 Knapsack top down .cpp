327a2bec0e4ad4a2c909d1a71bff89b6a5a7147f
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-10-04 18:28:54 +0600

    add

--- Semister 02/Algorithms/Week 05/Module 17/0-1 Knapsack top down .cpp
+++ Semister 02/Algorithms/Week 05/Module 17/0-1 Knapsack top down .cpp
@@ -35,7 +35,15 @@ int main(){
         cin>>value[i];
     }
     int w;cin>>w;
-    cout<<knapsack(n,weight,value,w);
+    cout<<knapsack(n,weight,value,w)<<endl;
+    for (int i = 0; i < 6; i++)
+    {
+        for (int j = 0; j < 6; j++)
+        {
+            cout<<memoization[i][j]<<" ";
+        }cout<<endl;
+        
+    }
     
     
     return 0;
