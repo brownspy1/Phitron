c619adbdba3020109aef884420fcb551a3168077
M.Mahadi <89521172+brownspy1@users.noreply.github.com>
2024-10-15 20:55:56 +0600

    add

---
+++ Semister 02/Algorithms/live for Contest class/XPSC Contest - Batch 04/Babul_s_Vowels.c
@@ -0,0 +1,15 @@
+// Created by M.Mahadi on 2024-10-14 Time:22:15:52
+#include <stdio.h>
+int main(){
+    char x;scanf("%c",&x);
+    if (x == 'a' || x == 'e' || x == 'i'|| x == 'o' || x =='u')
+    {
+        printf("Vowel");
+    }else
+    {
+        printf("Consonant");
+    }
+    
+    
+    return 0;
+}
